1dfc571a14e5742f14f63b111f40dc700a180b88
mikleikleek <n6pami00@students.oamk.fi>
2019-01-14 00:32:41 +0200

    First Commit
    Lets see how this works. I'm trying to get my Unity project to Github

---
+++ Tower Defence/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public static int enemiesAlive = 0;
+
+    public Wave[] waves;
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    public GameManager gameManager;
+
+    private int waveIndex = 0;
+
+    void Start () {
+        enemiesAlive = 0;
+    }
+
+    void Update () {
+        if (enemiesAlive > 0) {
+            return;
+        }
+
+        if (waveIndex == waves.Length) {
+            gameManager.WinLevel();
+            this.enabled = false;
+        }
+
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            return;
+        }
+
+        countdown -= Time.deltaTime;
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+        waveCountdownText.text = string.Format("{0:00.00}", countdown);
+    }
+
+    IEnumerator SpawnWave () {
+        PlayerStats.Rounds++;
+
+        Wave wave = waves[waveIndex];
+
+        enemiesAlive = wave.count;
+
+        for (int i = 0; i < wave.count; i++) {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        waveIndex++;
+    }
+
+    void SpawnEnemy (GameObject enemy) {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+}
